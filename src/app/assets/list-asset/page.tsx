b410b8969ab7ed7201658acbd7670c6ed98d97cc
Olanrewaju Alaba <alabaolanrewaju13@gmail.com>
2025-02-21 02:32:34 +0100

    navbar updated, asset litsting and update profile page created, function to get auth user information created

---
+++ src/app/assets/list-asset/page.tsx
@@ -0,0 +1,19 @@
+"use client";
+import React, { useEffect } from "react";
+import withAuth from "../../context/withAuth";
+import AssetListingForm from "../../components/AssetListingForm";
+import getAuthUser from "@/lib/getAuthUser";
+
+
+const ListAssetPage = () => {
+    const user = getAuthUser();
+    
+    return (
+        <div>
+            <AssetListingForm />
+        </div>
+    );
+
+};
+
+export default withAuth(ListAssetPage);
